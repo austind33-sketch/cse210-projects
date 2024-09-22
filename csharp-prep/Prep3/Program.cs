a892e8ac988c892ee7e431046faf4980dbf08b64
Austin Densley <austin.densley00@gmail.com>
2024-09-21 23:26:55 -0600

    Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        { 
+             Console.Write("What is the number I'm thinking of?");
+            int.Parse(Console.ReadLine());
+
+            if (magicNumber == guess)
+            {
+                Console.Write("Correct!! It was" [magicNumber]);
+            }
+            else
+            {
+                Console.Write("Nope! Guess again");
+            }
+        }
     }
 }
