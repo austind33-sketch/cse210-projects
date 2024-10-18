f1a0b18a31fc5bbbdc333dfbb6a782e646461919
Austin Densley <austin.densley00@gmail.com>
2024-10-17 23:26:06 -0600

    LearningW05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        MathAssignment mathAssignment = new MathAssignment("Jared Barney", "Algebra", "5.1", "10-15");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Dylan Wheeler", "Literature", "The Greatest Salesman on Earth Analysis");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
