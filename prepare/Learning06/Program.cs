82f73ace7f3a1f4e270844fa5b3172718cbca7c4
Austin Densley <austin.densley00@gmail.com>
2024-10-24 23:35:52 -0600

    pREP6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning06 World!");
+        DisplayMessage();
+
+        string userName = AskForUserName();
+        int userNumber = AskForFavoriteNumber();
+
+        int squaredNumber = CalculateSquare(userNumber);
+
+        ShowResult(userName, squaredNumber);
+    }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to my Program!");
+    }
+
+    static string AskForUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int AskForFavoriteNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int CalculateSquare(int number)
+    {
+        return number * number;
+    }
+
+    static void ShowResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your favorite number is {squaredNumber}!");
+        Console.WriteLine("Have a nice day!");
     }
 }
